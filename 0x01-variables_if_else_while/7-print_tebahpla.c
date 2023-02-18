197a5c3ac7a0fd2928d7812cefdda319d181a3cf
Treasure <treshy2005@gmail.com>
2023-02-17 17:52:51 -0800

    We thank God-5

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase reversely.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int num;
+for (num = 25; num <= 0; num--)
+{
+putchar(alphabet[num]);
+}
+putchar('\n');
+return (0);
+}
