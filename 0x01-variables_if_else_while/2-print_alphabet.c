5b33f258fc338bb08d59aa661c7879015437e087
Treasure <treshy2005@gmail.com>
2023-02-17 16:40:01 -0800

    We thank God

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,9 @@ int main(void)
 {
 char letters[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for (i = 0; i <= 26; i++)
+for (i = 0; i < 26; i++)
 {
-putchar(i);
+putchar(letters[i]);
 }
 putchar(n);
 return (0);
