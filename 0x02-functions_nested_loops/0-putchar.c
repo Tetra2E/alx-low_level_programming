9c47947b930cae6cdd3800213e1127b50543b671
Treasure <treshy2005@gmail.com>
2023-02-21 09:35:03 -0800

    Getting better at C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
-#ifndef Main_h
-#define Main_h
-
-/*
- * File: main.h
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
+#include "main.h"
 
 /**
- * _putchar - Writes the character c to std
- * @c: Parameter to compare
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
  */
-int _putchar(char c);
+int main(void)
+{
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-#endif
+return (0);
+}
