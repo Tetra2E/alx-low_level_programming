74f1135aee6108337af15340115a71a8ec2dc240
Treasure <treshy2005@gmail.com>
2023-02-21 12:26:47 -0800

    Getting better at C-0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,20 @@ void print_alphabet(void)
 {
 char letter;
 
-for (letter = a; letter <= z; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 
-_putchar(n);
+_putchar('\n');
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
